76cc6013487cd1f27bcac1745e61e553917506ac
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-01 16:29:26 +0530

    basiccpp

---
+++ Basics/first.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world"<<endl;
+    cout<<"jai sree ram"<<endl;
+    cout<<"jai sri krishna"<<endl<<"radharadha"<<endl;
+    cout<<3+2<<endl;
+    int a;
+    a = 90; //variable defined
+    int b = 89;
+    cout<<a<<endl;
+    int c = a+b;
+    cout<<c<<endl;
+    char f = 'i';
+    cout<<f<<endl;
+    float g = 9.8;
+    cout<<g<<endl;
+    bool r = 0;
+    cout<<r<<endl;
+};
