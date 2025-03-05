2f2e1940fefe10dbece7ba255bcae5f7e8bd143d
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-05 07:59:38 +0530

    ifelsecondns

---
+++ Basics/third.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int marks;
+    // cin>>marks;
+    // if(marks>33)
+    // {
+    //     cout<<"pass";
+    // }
+    // else{
+    //     cout<<"fail";
+    // }
+
+    // even or odd
+    // int num;
+    // cout<<"enter ur number";
+    // cin>>num;
+    // if(num%2 == 0)
+    // {
+    //     cout<<"even";
+    // }
+    // else
+    // {
+    //     cout<<"odd";
+    // }
+
+    // voting system
+
+    // int age;
+    // cout<<"enter ur age";
+    // cin>>age;
+    // if(age>18)
+    // {
+    //     cout<<"ur a adult";
+    // }
+    // else
+    // {
+    //     cout<<"ur a teen";
+    // }
+}
