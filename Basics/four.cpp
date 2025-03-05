9aa6c44570fd8a5fd93ac294235c00d4644c5592
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-05 09:07:15 +0530

    loops

---
+++ Basics/four.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // for(int i = 1 ; i <= 10 ; i++)
+    // {
+    //     cout<< "hello\n";
+    // }
+
+    //  print n natural numbers and square's code
+    int n;
+    cout<<"enter number";
+    cin>>n;
+
+    // for(int i = 1; i<=n; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i = 1; i<=n; i++)
+    // {
+    //     cout<<i<<"square is: " <<i*i<<endl;
+    // }
+
+    // print all even numbers until 20
+
+    // for (int i = 2; i<=n ; i = i+2)
+    // {
+    //     cout<<i;
+    // }
+    // odd
+    // for (int i = 1; i<=n ; i = i+2)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // divisible by 4 my code
+
+    for (int i = 1; i<=n ; i= i+4)
+    {
+        cout<<i<<endl;
+    }
+
+    // gpt code
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 4 == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
