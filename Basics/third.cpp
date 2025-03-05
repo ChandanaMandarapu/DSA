2833eb05904189029a46fe00bc451bd1198ed6d1
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-05 08:11:56 +0530

    if else with exampels done

--- Basics/third.cpp
+++ Basics/third.cpp
@@ -38,4 +38,56 @@ int main()
     // {
     //     cout<<"ur a teen";
     // }
+
+    // else-if conditionals
+    // we can also remove braces for singlecondns
+    // int n;
+    // cout<<"enter n";
+    // cin>>n;
+
+    // if(n > 0)
+    // cout<<"n is positive";
+
+    // else if(n == 0)
+    // cout<<"zero";
+
+    // else
+    // cout<<"negative";
+
+    // vowel or not
+
+    // char c = 'a';
+    // if(c == 'a')
+    // cout<<"vowel";
+    // else if(c == 'e')
+    // cout<<"vowel";
+    // else if(c == 'i')
+    // cout<<"vowel";
+    // else if(c == 'o')
+    // cout<<"vowel";
+    // else if(c == 'u')
+    // cout<<"vowel";
+    // else 
+    // cout<<"constant";
+
+    // weekdays
+
+    int days;
+    cin>>days;
+    if(days==1)
+    cout<<"monday";
+    else if(days==2)
+    cout<<"tuesday";
+    else if(days==3)
+    cout<<"wednesday";
+    else if(days==4)
+    cout<<"thursday";
+    else if(days==5)
+    cout<<"friday";
+    else if(days==6)
+    cout<<"saturday";
+    else if(days==7)
+    cout<<"sunday";
+    else
+    cout<<"nothing";
 }
