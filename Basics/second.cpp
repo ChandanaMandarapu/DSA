bafdc5e7daf42b376049814d9df2cbc882c7a138
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-05 07:38:23 +0530

    typecasting

--- Basics/second.cpp
+++ Basics/second.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 int main()
 {
-    int a,b;
-    cout<<"enter your first number : ";
-    cin>>a;
-    cout<<"enter your second number : ";
-    cin>>b;
-    cout<<a-b;
+    // int a,b; - taking an input
+    // cout<<"enter your first number : ";
+    // cin>>a;
+    // cout<<"enter your second number : ";
+    // cin>>b;
+    // cout<<a-b;
+    
+    // typecasting
+    int i = 19;
+    char j = 'c';
+    // i = j;
+    j = i;
+    cout<<j;
 }
