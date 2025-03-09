e878461af7a367382be7edaa51f4f5f24e984ba7
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-10 05:24:17 +0530

    correct approaches

--- first.cpp
+++ first.cpp
@@ -67,18 +67,41 @@ int main () {
 
     // fibanocci series
 
-    int n; 
+    //  my approach
 
-    cout << "enter a number : ";
+    // int n; 
 
+    // cout << "enter a number : ";
+
+    // cin >> n;
+
+    // int currentValue, prevValue = 1, lastValue = 0;
+
+    // for ( int i = 1; i<= currentValue ; i++) {
+    //     currentValue = prevValue + lastValue;
+    //     cout << currentValue;
+    //     lastValue = prevValue + currentValue;
+    //     cout << lastValue;
+    // }
+
+    // correct apporach
+
+    int n;
+
+    cout << "Enter a number: ";
     cin >> n;
 
-    int currentValue, prevValue = 1, lastValue = 0;
+    int prevValue = 0, currentValue = 1, nextValue;
 
-    for ( int i = 1; i<= currentValue ; i++) {
-        currentValue = prevValue + lastValue;
-        cout << currentValue;
-        lastValue = prevValue + currentValue;
-        cout << lastValue;
+    cout << prevValue << " " << currentValue << " ";
+
+    for (int i = 2; i < n; i++) {  // Start from 2 because first two are already printed
+        nextValue = prevValue + currentValue;
+        cout << nextValue << " ";
+
+        prevValue = currentValue;
+        currentValue = nextValue;
     }
+
+    return 0;
 }
