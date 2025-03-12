fe8833375e3585f61584929c87d9a06c579fe896
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-12 15:20:14 +0530

    arrays

---
+++ Arrays/first.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    // declaration of arrays
+    int arr [5] = {1,2,3,4,5};
+    for (int i = 0; i<5; i++){
+        cout << arr[i] << " ";
+        return 0;
+    }
+    // int arr2[6] = [0];
+
+    int arr[3];
+    for (int i=0;i<6;i++){
+        cin >> arr[i];
+    }
+}
