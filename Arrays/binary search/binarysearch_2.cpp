962f79d208447b4a04646c3b7bf46423aa1e3517
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-17 13:19:56 +0530

    binary search dec order approach

---
+++ Arrays/binary search/binarysearch_2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int BinarySearch(int arr[], int n, int key)
+{
+    int start = 0, end = n - 1, mid;
+
+    while (start <= end)
+    {
+
+        mid = start + (end-start)/2;
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int arr[10];
+    int n;
+    cout << "enter the number of elements in array";
+    cin >> n;
+    cout << "enter the elements ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int key;
+    cout << "enter the key";
+    cin >> key;
+
+    // funcn to implement
+
+    cout << BinarySearch(arr, n, key);
+    return 0;
+}
