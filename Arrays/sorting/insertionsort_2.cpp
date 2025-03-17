a25881eba78fac20e8d9bb8264427b5304adb204
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-17 07:09:33 +0530

    insertion sort decreasing order

---
+++ Arrays/sorting/insertionsort_2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10];
+    int n;
+    cout << "enter size of array ";
+    cin >> n;
+    cout << "enter elements in array ";
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j < 0; j--)
+        {
+
+            if (arr[j] > arr[j - 1])
+                swap(arr[j], arr[j - 1]);
+
+            else
+                break;
+        }
+    }
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
