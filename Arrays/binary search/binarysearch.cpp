bef223c9179a661ac40e5979c650b1d1e5d076c7
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-17 12:50:54 +0530

    final changes

--- Arrays/binary search/binarysearch.cpp
+++ Arrays/binary search/binarysearch.cpp
@@ -2,41 +2,46 @@
 
 using namespace std;
 
+int BinarySearch(int arr[], int n, int key)
+{
+    int start = 0, end = n - 1, mid;
 
-int BinarySearch ( int arr[] , int n , int key){
-    int start =0, end = n-1 , mid;
+    while (start <= end)
+    {
 
-    while (start <= end){
+        mid = start + end / 2;
 
-        mid = start + end / 2 ;
-
-        if (arr[mid] == key) {
+        if (arr[mid] == key)
+        {
             return mid;
         }
-        else if (arr [mid] < key){
-            start = mid +1;
+        else if (arr[mid] < key)
+        {
+            start = mid + 1;
         }
-        else {
+        else
+        {
             end = mid - 1;
         }
     }
     return -1;
 }
-int main (){
-    int arr [10];
+int main()
+{
+    int arr[10];
     int n;
     cout << "enter the number of elements in array";
     cin >> n;
     cout << "enter the elements ";
-    for (int i=0; i<n; i++)
-    cin >> arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
     int key;
     cout << "enter the key";
-    cin >>key;
+    cin >> key;
 
-    // funcn to implement 
+    // funcn to implement
 
-    cout << BinarySearch(arr , n, key);
+    cout << BinarySearch(arr, n, key);
     return 0;
 }
