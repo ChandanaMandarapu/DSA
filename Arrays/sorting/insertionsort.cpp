20c1a2d4f1339ec77f7a67c1fbf3186717f39eb4
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-17 07:07:48 +0530

    update

--- Arrays/sorting/insertionsort.cpp
+++ Arrays/sorting/insertionsort.cpp
@@ -2,26 +2,29 @@
 
 using namespace std;
 
-int main (){
+int main()
+{
     int arr[10];
     int n;
     cout << "enter size of array ";
     cin >> n;
     cout << "enter elements in array ";
 
-    for (int i =0; i<n; i++)
-    cin >> arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
-    for ( int i = 1; i<n; i++){
-        for ( int j = i;  j<0 ; j--){
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j < 0; j--)
+        {
 
-            if (arr [j] < arr [j-1])
-            swap (arr[j] ,arr[j-1]);
+            if (arr[j] < arr[j - 1])
+                swap(arr[j], arr[j - 1]);
 
-            else 
-            break;
+            else
+                break;
         }
     }
-    for (int i = 0; i<n; i++)
-    cout << arr[i] << " ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 }
