f1eb9072d485bc0912735e762a5788fe830088fc
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-27 05:43:21 +0530

    modified

--- 2DArrays/first.cpp
+++ 2DArrays/first.cpp
@@ -2,24 +2,25 @@
 
 using namespace std;
 
-void printcol (int arr [][3] , int row, int col)
+void printcol(int arr[][3], int row, int col)
 {
-    for (int j=0;j<row;j++)
-    for (int i=0;i<col;i++)
-    cout << arr[i][j] << " ";
+    for (int j = 0; j < row; j++)
+        for (int i = 0; i < col; i++)
+            cout << arr[i][j] << " ";
 }
 
-int main(){
+int main()
+{
     // creating and initialising a 2d array
 
-    int arr[4][3] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    int arr[4][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
     // printing
 
-    for(int row = 0; row<4; row++)
+    for (int row = 0; row < 4; row++)
     {
-        for (int col=0; col<3; col++)
-        cout<<arr[row] [col] << " ";
+        for (int col = 0; col < 3; col++)
+            cout << arr[row][col] << " ";
     }
 
     // printing all the value in array col wise using funcn call
@@ -30,11 +31,13 @@ int main(){
 
     int x = 7;
 
-    for(int row = 0; row <4; row++){
-        for (int col = 0; col<3;col++){
-            if(arr[row][col] == x)
+    for (int row = 0; row < 4; row++)
+    {
+        for (int col = 0; col < 3; col++)
+        {
+            if (arr[row][col] == x)
             {
-                cout<< "yes";
+                cout << "yes";
                 return 0;
             }
         }
