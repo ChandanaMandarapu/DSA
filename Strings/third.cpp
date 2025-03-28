c3e156dcb62dca6d7afdfe58ee4e4b924e8f4899
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-03-28 17:44:20 +0530

    palindrome

---
+++ Strings/third.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    string s = "naman";
+    int start = 0 , end = s.size()-1; 
+
+    while(start<end){
+
+    if(s[start] != s[end])
+    cout << "not a palindrom";
+    return 0;
+
+    start++, end--;
+    }
+
+    cout << "its a palindrome";
+}
