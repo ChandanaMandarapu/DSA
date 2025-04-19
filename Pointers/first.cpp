7530ebb90432a9d273e5c6b86ee87a46791bf43b
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-04-20 04:53:57 +0530

    pointers

---
+++ Pointers/first.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    // pringing address
+    cout << &a << endl;
+    int *ptr = &a;
+    cout << ptr << endl;
+    cout << sizeof(ptr);
+    float m = 2.6;
+    float *ptr1 = &m;
+    cout <<ptr1 << endl;
+}
